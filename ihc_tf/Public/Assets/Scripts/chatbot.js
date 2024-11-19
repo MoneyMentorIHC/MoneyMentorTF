fd48a996753a7c1f500bcf4805df571adcf49504
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2024-11-19 14:10:00 -0500

    Update chatbot.js
    dark mode listo

--- ihc_tf/Public/Assets/Scripts/chatbot.js
+++ ihc_tf/Public/Assets/Scripts/chatbot.js
@@ -1,20 +1,22 @@
 const sideMenu = document.querySelector('aside');
 const menuBtn = document.getElementById('menu-btn');
-const closeBtn = document.getElementById('close-btn');
-
 const darkMode = document.querySelector('.dark-mode');
 
-menuBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'block';
-});
 
-closeBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'none';
+menuBtn.addEventListener('click', () => {
+  // Si el sidebar está visible, lo oculta; si está oculto, lo muestra
+  if (sideMenu.style.display === 'block') {
+      sideMenu.style.display = 'none'; // Oculta el menú
+  } else {
+      sideMenu.style.display = 'block'; // Muestra el menú
+  }
 });
+ 
 
 darkMode.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode-variables');
     darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
     darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
 })
+
 
